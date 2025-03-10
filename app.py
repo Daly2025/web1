0b452a36dc4d3af4c0f57196d1991d3e86a55566
Daly2025 <lydiafdez84@gmail.com>
2025-03-10 17:08:27 +0100

    ay

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
-    return render_template('index.html')
+def index ():
+    return render_template ('index.html')
 
-@app.route('/hola')
-def index():
-    return "¡Hola desde Flask!"
-
-@app.route('/hello')
-def hello_world():
-    return "Hello, world!"
+@app.route('/saludo/<nombre>/<int:edad>')
+def saludo(nombre,edad):
+    if(edad<18):
+        return 'Hola ' + nombre + "eres mayor de edad"
+    else:
+        return 'Hola ' + nombre + "eres menor de edad"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
