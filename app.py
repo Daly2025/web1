acbdbed75ecc1b46655323f7ab08d42ce4844068
Daly2025 <lydiafdez84@gmail.com>
2025-03-10 19:04:18 +0100

    guardar sesion

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template,request
+from flask import Flask,render_template,request,redirect,url_for,session
 
 app = Flask(__name__)
+app.secret_key="123456"
 
-@app.route('/')
-def index ():
-    return render_template ('index.html')
+@app.route('/login',methods=['GET'])
+def index():
+    return render_template("index.html")
 
+#recibir dos parametros nombre y edad
 @app.route('/saludo/<nombre>/<int:edad>')
-def saludo(nombre,edad):
+def prueba(nombre,edad):
     if(edad<18):
-        return 'Hola ' + nombre + "eres mayor de edad"
+        return "Hola "+nombre+" eres menor de edad"
     else:
-        return 'Hola ' + nombre + "eres menor de edad"
-    
-    #login post
-@app.route('/login', methods=['POST'])
-def login():
-    #obtener datos del formulario
-    username = request.form('username')
-    password = request.form('password')
-    return f"nombre: {username} pass: {password}"
+        return "Hola "+nombre+" eres mayor de edad"
 
-if __name__ == '__main__':
-    app.run(debug=True, port=80)
+#login post
+@app.route('/login',methods=['POST'])
+def login():
+    #obtener los datos del formulario
+    username = request.form['username'] 
+    password = request.form['password']
+    if(username == 'admin' and password == 'admin'):
+        #guardar datos en session
+        session['username'] = username
+        return redirect(url_for('admin'))
+
+    else:
+         return render_template("index.html",mensaje="Usuario o contraseña incorrecta")
+
+    
+    
+@app.route('/admin',methods=['GET'])
+def admin():
+    if "username" in session:
+        return render_template("admin/admin.html")
+    else:
+        return redirect(url_for('login'))
+
+if __name__ == '__main__':    
+    app.run(debug=True,port=80)
+    
